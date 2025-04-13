4e9ec8d2dbf343c8481253701b28aa3df8de4893
Zawra <nouhatechini@gmail.com>
2025-04-13 19:57:05 +0100

    front dev

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit } from '@angular/core'; 
+import { Component, OnInit } from '@angular/core';
 import { SoftpayService } from '../services/softpay.service';
 import { ContractService } from '../services/contract.service';
+import { Payment } from '../models/payment.model';
+import { Contract } from '../models/contract.model';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
   totalPayments: number = 0;
@@ -18,16 +19,12 @@ export class DashboardComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.SoftpayService.getPayments().subscribe(data => {
+    this.SoftpayService.getPayments().subscribe((data: Payment[]) => {
       this.totalPayments = data.reduce((sum, payment) => sum + payment.amount, 0);
     });
 
-    this.contractService.getContracts().subscribe((data: number): void => {
-      this.totalContracts = data;
+    this.contractService.getContracts().subscribe((data: Contract[]) => {
+      this.totalContracts = data.length;
     });
   }
 }
-
-
-  
-
